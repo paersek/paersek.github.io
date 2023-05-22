e264a983748c7258ebe475148e98ceb280427a77
paersek <133062452+paersek@users.noreply.github.com>
2023-05-21 20:51:59 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ export default function HomePage() {
           style={{ aspectRatio: '9 / 13' }}
         />
         <Image src={DavidImage} alt="David" />
+        <Image src={PeakSeason} alt="PeakSeason" />
       </Container>
 
       <Container orientation="vertical" gap={10}>
