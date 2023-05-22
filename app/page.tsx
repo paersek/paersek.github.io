eb07b7b80147e2805a9605a1adab563be37b669e
paersek <133062452+paersek@users.noreply.github.com>
2023-05-21 23:56:03 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -22,17 +22,6 @@ export default function HomePage() {
         />
         <Image src={DavidImage} alt="David" />
         <Image src={DavidImage2} alt="David" />
-         </Container>
-       <Container orientation="vertical" gap={0}>
-          <div>PRESS:</div>
-          <a
-            href="https://www.adweek.com/media/tiktok-debuts-tiktok-creator-portal/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            AdWeeK: TikTok Debuts Creator Portal
-          </a>
-        </Container>
       </Container>
        
       <Container orientation="vertical" gap={10}>
