5dfeec0318916c7924a4f47018a2359acd10f4b4
paersek <133062452+paersek@users.noreply.github.com>
2023-05-21 22:19:11 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import {
   SectionTitle,
   VimeoEmbed
 } from 'app/components';
+import DavidImage from 'app/images/DavidWordMarkWithField6.png';
 import DavidImage2 from 'app/images/PeakSeasonPoster_WEB_5-21v3.png';
 
 export const metadata = {
@@ -19,7 +20,7 @@ export default function HomePage() {
           src="https://mentalcanvas.com/vm/tr7fbzu/scene/"
           style={{ aspectRatio: '9 / 13' }}
         />
-        <Image src={DavidImage2} alt="David" />
+        <Image src={DavidImage} alt="David" />
         <Image src={DavidImage2} alt="David" />
       </Container>
       
