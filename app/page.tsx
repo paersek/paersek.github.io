55f128228d556b1cda17c38d09d77263f35d57fb
paersek <133062452+paersek@users.noreply.github.com>
2023-05-21 22:06:13 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ export default function HomePage() {
           style={{ aspectRatio: '9 / 13' }}
         />
         <Image src={DavidImage} alt="David" />
+        <Image src={DavidImage} alt="David" />
       </Container>
       
       
